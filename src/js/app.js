b2585949c078399625afc2e513089b420ec742e2
Denis Chernousov <chernousovdv@gmail.com>
2024-03-07 16:07:07 +0700

    layout hero

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,26 @@ const isMobile = document.documentElement.clientWidth < 768;
 const isTablet = document.documentElement.clientWidth < 1140;
 
 document.addEventListener('DOMContentLoaded', (event) => {
-   
-    
+    document.getElementById('catalog-btn').onclick = function (event) {
+        event.stopPropagation(); // Остановить всплытие события, чтобы клик на кнопке не срабатывал на внешний обработчик
+        document.getElementById('catalog').classList.toggle('catalog-open');
+        document.getElementById('dropIconTog').classList.toggle('btn-reverse');
+    };
 
+    const catalogBlock = document.getElementById('catalog');
 
+    document.addEventListener('click', function (event) {
+        const catalogElement = document.getElementById('catalog');
+        const targetElement = event.target;
 
+        if (
+            !catalogElement.contains(targetElement) &&
+            !targetElement.matches('#catalog-btn')
+        ) {
+            catalogElement.classList.remove('catalog-open');
+            document
+                .getElementById('catalog-btn')
+                .classList.remove('btn-reverse');
+        }
+    });
 });
-document.getElementById('catalog-btn').onclick = function(){
-    document.getElementById('catalog').classList.toggle('catalog-open')
-}
