b9c66605d0f96d3b9213b873bd4bcf7c62fff6a3
Denis Chernousov <chernousovdv@gmail.com>
2024-03-11 17:04:30 +0700

    layout

--- src/js/app.js
+++ src/js/app.js
@@ -25,3 +25,78 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 });
+
+//////////////////////// это верстальщетские скрипты, что бы не плодить портянки
+
+const catalog = [
+    {
+        title: 'Оборудование для деревообработки',
+        icon: 'icon-derevo.svg',
+    },
+    {
+        title: 'Оборудование для производства бетона и бетонных изделий',
+        icon: 'icon-beton.svg',
+    },
+    {
+        title: 'Строительное оборудование',
+        icon: 'icon-derevo.svg',
+    },
+    {
+        title: 'Сельскохозяйственное оборудование',
+        icon: 'icon-derevo.svg',
+    },
+    {
+        title: 'Оборудование для металлообработки',
+        icon: 'icon-derevo.svg',
+    },
+    {
+        title: 'Оборудование для нанесения пенополиуретанового уплотнения',
+        icon: 'icon-derevo.svg',
+    },
+    {
+        title: 'Экструзионное оборудование',
+        icon: 'icon-derevo.svg',
+    },
+    {
+        title: 'Упаковочное оборудование',
+        icon: 'icon-derevo.svg',
+    },
+    {
+        title: 'Пищевое оборудование',
+        icon: 'icon-derevo.svg',
+    },
+    {
+        title: 'Автоматизация производства',
+        icon: 'icon-derevo.svg',
+    },
+];
+
+const catalogItems = catalog.map((item) => {
+    return `<a href="#" class="hero__catalog-item  p-lg-3 py-lg-4 me-lg-4  mb-lg-4 col-lg-3 "> 
+                      <div class="col-lg-12">
+                          <h3 class="hero__catalog-text col-lg-7">
+                              ${item.title}
+                          </h3>
+                      </div>
+                      <div class="col-lg-12 d-flex">
+                          <div class="hero__catalog-item-arrow-icon col-lg-6 d-flex align-items-end">
+                              <img src="images/icon/arow-tuda-green.svg" />
+                          </div>
+                          <div class="hero__catalog-item-prod-icon col-lg-6 d-flex justify-content-end">
+                              <img
+                              class="hero__catalog-item-prod-icon-un"
+                                  src="images/icon/catalog/${item.icon}"
+                                  alt=""
+                              />
+                              <img
+                              class="hero__catalog-item-prod-icon-hover"
+                                  src="images/icon/catalog/h-${item.icon}"
+                                  alt=""
+                              />
+                          
+                      </div>
+                  </div></a>`;
+});
+
+document.querySelector('.hero__catalog-items ').innerHTML =
+    catalogItems.join('');
