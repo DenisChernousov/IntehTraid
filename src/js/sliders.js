6d9ec885147648c2cba74ef7cf0e05dd89e3e4ab
Denis Chernousov <chernousovdv@gmail.com>
2024-03-15 14:50:26 +0700

    футер

---
+++ src/js/sliders.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.running-line', {
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    simulateTouch: false,
+    loop: true,
+    speed: 5000,
+    autoplay: {
+        delay: 0,
+        reverseDirection: false,
+        disableOnInteraction: true,
+    },
+});
